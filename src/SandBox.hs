7371ee2918f35584a92d07450adec17c9e9ff9d1
Hajime Suzuki <hajime.doublebass@gmail.com>
2020-06-29 08:15:51 +0200

    wip: implement MonadUnliftIO for UseCase

--- src/SandBox.hs
+++ src/SandBox.hs
@@ -1,7 +1,8 @@
 
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE ConstrainedClassMethods  #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module SandBox where
 
@@ -13,15 +14,18 @@ import           Network.AWS                    ( runAWS
                                                 )
 import           Network.AWS.DynamoDB.ListTables
 import           Control.Monad.Trans.AWS
+import           Control.Monad.Trans.Except
 import           Types
 import           Control.Lens
 import           Config
 import           Control.Monad.Catch
+import           Control.Monad.Except
 import           Repositories.Common
 import           Domain.Order
 import           Data.Aeson                     ( encode )
 import           Data.Either.Validation
 import           Repositories.Types
+import           Repositories.Order
 
 getTablesTest :: Repository' ()
 getTablesTest = do
@@ -44,3 +48,36 @@ domainTest = do
     case order of
         Failure e     -> pPrint e
         Success order -> pPrint $ encode order
+
+
+data ERR = Error1 | Error2 deriving (Show)
+instance Exception ERR
+
+newtype UC a = UC {
+  unUC ::  ReaderT Config (ExceptT ERR IO) a -- TODO: type exception
+} deriving (Functor, Applicative, Monad, MonadIO, MonadReader Config,MonadCatch, MonadThrow, MonadError ERR) -- TODO: add MonadUnliftIO
+
+instance MonadUnliftIO UC where
+    askUnliftIO = return $ UnliftIO
+        (\uc -> do
+            config <- getConfig
+            v      <- runUC config uc
+            case v of
+                Left  e -> throwM e
+                Right a -> return a
+        )
+
+
+runUC :: Config -> UC a -> IO (Either ERR a)
+runUC r = runExceptT . flip runReaderT r . unUC
+
+testUseCase :: IO ()
+testUseCase = do
+    config <- getConfig
+    v      <- runUC config $ do
+        pPrint "use case"
+        a <- getByOrderId "1"
+        pPrint a
+        throwError Error1
+        return a
+    pPrint v
