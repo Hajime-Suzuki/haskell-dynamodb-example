7c0a241ef4e8aad816a480e28c2ccaa0da516e76
Hajime Suzuki <hajime.doublebass@gmail.com>
2020-07-07 21:55:50 +0200

    throw error from adapter

--- src/Adapters/Order.hs
+++ src/Adapters/Order.hs
@@ -16,40 +16,51 @@ import           Adapters.Types
 import           App
 import           Data.Aeson.Embedded
 
-
 getOrderAdapter :: Adapter () Value
 getOrderAdapter req = do
   config <- getConfig
-  let orderId = fromMaybe (error "id not found")
-                          (lookup "id" $ req ^. agprqPathParameters)
-  mayOrder <- runUseCase config (getOrderUseCase orderId)
-  handleResponse "order" mayOrder
+  let mayOrderId = lookup "id" $ req ^. agprqPathParameters
+  case mayOrderId of
+    Nothing      -> throwString "userId not found in pathParams"
+    Just orderId -> do
+      mayOrder <- runUseCase config (getOrderUseCase orderId)
+      handleResponse "order" mayOrder
 
 createOrderAdapter :: Adapter CreateOrderPayload Value
 createOrderAdapter req = do
   config <- getConfig
-
-  let payload = fromJust $ req ^. requestBodyEmbedded -- TODO: add exception
-
-  order <- runUseCase config $ createOrderUseCase payload
-  handleResponse "order" order
+  let mayPayload = req ^. requestBodyEmbedded
+  case mayPayload of
+    Nothing      -> throwString "body is empty"
+    Just payload -> do
+      order <- runUseCase config $ createOrderUseCase payload
+      handleResponse "order" order
 
 getOrderByUserIdAdapter :: Adapter () Value
-getOrderByUserIdAdapter req = do
+getOrderByUserIdAdapter req = errorHandler $ do
   config <- getConfig
-  let userId = fromMaybe (error "id not found")
-                         (lookup "id" $ req ^. agprqPathParameters)
-  mayOrders <- runUseCase config (getOrdersByUserIdUseCase userId)
-  handleResponse "orders" mayOrders
+  let mayUserId = (lookup "id" $ req ^. agprqPathParameters)
+
+  when (isNothing mayUserId) $ throwString "userId not found"
+
+  case mayUserId of
+    Nothing     -> throwString "userId not found in pathParams"
+    Just userId -> do
+      mayOrders <- runUseCase config (getOrdersByUserIdUseCase userId)
+      handleResponse "orders" mayOrders
 
 updateStatusAdapter :: Adapter UpdateStatusPayload Value
 updateStatusAdapter req = errorHandler $ do
   config <- getConfig
-  let payload = fromJust $ req ^. requestBodyEmbedded
-      orderId = fromMaybe (error "id not found")
-                          (lookup "id" $ req ^. agprqPathParameters)
-  mayOrder <- runUseCase config $ updateStatusUseCase orderId payload
-  handleResponse "order" mayOrder
+  let (mayPayload, mayOrderId) =
+        (req ^. requestBodyEmbedded, lookup "id" $ req ^. agprqPathParameters)
+
+  case (mayPayload, mayOrderId) of
+    (Nothing, _) -> throwString "body is empty"
+    (_, Nothing) -> throwString "order id is not found in pathParams"
+    (Just payload, Just orderId) -> do
+      mayOrder <- runUseCase config $ updateStatusUseCase orderId payload
+      handleResponse "order" mayOrder
 
 handleResponse
   :: (ToJSON a, ToJSON e, Exception e)
@@ -61,6 +72,7 @@ handleResponse _ (Left e) = return $ throw e
 handleResponse key (Right val) =
   return $ response 500 & responseBodyEmbedded ?~ object [(key, toJSON val)]
 
+-- TODO: wrap this with ExceptT or EitherT
 errorHandler
   :: (MonadUnliftIO m)
   => m (APIGatewayProxyResponse (Embedded Value))
@@ -72,3 +84,4 @@ errorHandler = flip
     return $ response 500 & responseBodyEmbedded ?~ object
       [("error", toJSON $ tshow e)]
   )
+
